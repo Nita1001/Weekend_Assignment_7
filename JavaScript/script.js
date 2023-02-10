fc1761f0dec73e7b59e855981d8dbfb7a249f4df
Nita1001 <devinedevops@gmail.com>
2023-02-10 22:32:52 +0200

    DataByCode from REST API

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,74 +1,71 @@
-let bigData = { 
+let bigData = {
     continents: [],
-    // countries: [],
+    countries: [],
 };
 
-
-function extractUniqueContinents(duplicatedContinents){
+function extractUniqueContinents(duplicatedContinents) {
     let newStr = duplicatedContinents.join(' ');
     newStr = newStr.split(' ');
     let uniqueContinents = [...new Set(newStr)];
-    uniqueContinents = uniqueContinents.filter((el) => el !== 'North' && el !== 'South');
-    bigData.continents.push(uniqueContinents); 
+    uniqueContinents = uniqueContinents.filter((el) => el !== 'North' && el !== 'South' && el !== 'Antarctica');
+    bigData.continents.push(uniqueContinents);
 }
 
-function getContinentName(data){
+function getContinentName(data) {
     let duplicatedContinents = [];
-    data.forEach( obj => {
+    data.forEach(obj => {
         duplicatedContinents.push(obj.continents);
     });
     extractUniqueContinents(duplicatedContinents);
 }
 
-async function getCountryName(){
 
-    let region = bigData.continents[0][4];
-    try{
-        const res = await fetch(`https://restcountries.com/v3.1/region/${region}/`);
-        if(res.ok){
-            const data1 = await res.json();
-            console.log('data1', data1);
-            data1.forEach((obj) => {
-                console.log(obj.population, obj.name.official);
-            })
-        }else {
-            throw new Error(res.status);
-        }
-  
-    }catch (err){
-        console.log('error:', err);
-    }
-    
-
-    // console.log('bigData continent', bigData.continents[0][0]);
-
-    // data.forEach( obj => {
-
-    //     if(obj.continents == bigData.continents[0][1]){
-    //         countries.push(obj.name.official);
-    //     }
-
-    // });
-    // console.log(`countries in ${bigData.continents[0][3]}`, countries);
-
-}
-
-
-async function getAllData(){
-    try{
-        const res = await fetch('https://restcountries.com/v3.1/all');
-        if(res.ok){
+// Countries & Cities
+// data.data code, country, iso3, populationCount {year: , value: }
+async function getAllData2() {
+    try {
+        const res = await fetch('https://countriesnow.space/api/v0.1/countries/population');
+        if (res.ok) {
             const data = await res.json();
-            getContinentName(data);
-            console.log(data);
-            getCountryName();
-        } else {
-            throw new Error(res.status);
+            console.log('Data API 1', data.data);
         }
-    } catch(err){
+    } catch (err) {
         console.log('Error: ', err);
     }
 }
 
+// REST API CODE
+async function getAllDataByCode(code) {
+    try {
+        const res = await fetch(`https://restcountries.com/v2/alpha/${code}`);
+        if (res.ok) {
+            const data = await res.json();
+            console.log('Data API 2', data);
+        }
+    } catch (err) {
+        console.log('Error: ', err);
+    }
+}
+
+// REST API ALL
+async function getAllData() {
+    try {
+        const res = await fetch('https://restcountries.com/v3.1/all');
+        if (res.ok) {
+            const data = await res.json();
+            console.log('REST API 2', data);
+
+            // getContinentName(data);
+            // getCountryName();
+            // getCitiesName();
+        }
+    } catch (err) {
+        console.log('Error: ', err);
+    }
+}
+
+// code from Countries & Cities API to find continent in REST API
+let code = 'NGA';
 getAllData();
-console.log('bigData: ', bigData);
+getAllData2();
+getAllDataByCode(code);
