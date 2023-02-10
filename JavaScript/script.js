3430c9460a9968ee804fc399a9cbdea04d36543d
Nita1001 <devinedevops@gmail.com>
2023-02-10 14:36:45 +0200

    extracting continents from API

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,28 @@
+let bigData = [];
 
 
+function extractUniqueContinents(duplicatedContinents){
+    let newStr = duplicatedContinents.join(' ');
+    newStr = newStr.split(' ');
+    let uniqueContinents = [...new Set(newStr)];
+    uniqueContinents = uniqueContinents.filter((el) => el !== 'North' && el !== 'South');
+    bigData.push(uniqueContinents); 
+}
+
+function getContinentName(data){
+    let duplicatedContinents = [];
+    data.forEach( obj => {
+        duplicatedContinents.push(obj.continents);
+    });
+    extractUniqueContinents(duplicatedContinents);
+}
 
 async function getAllData(){
     try{
         const res = await fetch('https://restcountries.com/v3.1/all');
         if(res.ok){
             const data = await res.json();
-            console.log(data);          
+            getContinentName(data);
         } else {
             throw new Error(res.status);
         }
@@ -16,4 +32,4 @@ async function getAllData(){
 }
 
 getAllData();
-
+console.log('bigData: ', bigData);
